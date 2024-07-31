5e9ca30bbcbb8d4c8278351ce9b30841facd1c09
Sahil <sahilgghh@gmail.com>
2024-07-31 11:14:42 +0530

    update backend url

--- src/util/info.js
+++ src/util/info.js
@@ -1,3 +1,7 @@
-const BASE_URL = "http://192.168.1.8:8080"
-const BASE_URL_WS = "ws://192.168.1.8:8080"
+//const BASE_URL = "http://192.168.1.8:8080"
+//const BASE_URL_WS = "ws://192.168.1.8:8080"
+
+const BASE_URL="https://chat-app-backend-production-0fc2.up.railway.app"
+const BASE_URL_WS="wss://chat-app-backend-production-0fc2.up.railway.app"
+
 export {BASE_URL,BASE_URL_WS};
